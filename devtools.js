7599141ac1b5c5201fa1c315cc2036bb18751192
lovelyhelperforyou <121383046+lovelyhelperforyou@users.noreply.github.com>
2022-12-25 17:40:00 +0530

    Add files via upload

--- devtools.js
+++ devtools.js
@@ -1,2 +1,2 @@
-chrome.devtools.panels.create("TUTORIEX", null, "panel.html", null);
-console.log("This is devtools script!");
+chrome.devtools.panels.create('MyPerfectice Fast', null, 'panel.html', null);
+console.log("This is devtools script!")
--- panel.js
+++ panel.js
@@ -1,20 +1,77 @@
-chrome.devtools.network.onRequestFinished.addListener(function(request){
-    if(request.request.url == "https://api-ng2.myperfectice.com/api/v1/learningTest/getQuestion"){
-        request.getContent(function(content){
-            var ans = JSON.parse(content);
-            console.log(ans.answers);
-            if (i != -1) {
-                console.log("Correct Answer is: " + i);
-            }
-        })
-    }
-}
-)
-function Correct(answers){
-    for(let i=0;i<answers.length;i++){
-        if(answers[i].isCorrectAnswer){
-            return i
+chrome.devtools.network.onRequestFinished.addListener(function (request) {
+  if (
+    request.request.url == 
+    "https://api-ng2.myperfectice.com/api/v1/learningTest/getQuestion"
+    
+    // "https://api-ng2.myperfectice.com/api/v1/learningTest/getPracticeSet/5da2351e42e6085038581511?referenceType=testseries&referenceId=5da2351f42e6085038581567"
+  ) {
+    // request.getContent(function (content, encoding) {
+    request.getContent(function (content) {
+      var myobj = JSON.parse(content);
+      console.log(myobj.answers);
+      let i = findCorrectAns(myobj.answers);
+      if (i != -1) {
+        console.log("Correct Answer is: " + i);
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {
+              msg: "Sending Data",
+              keys: i,
+            });
+          }
+        );
+      }
+    });
+  }
+});
+
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.msg == "startPanel") {
+    console.log("now I will get started in the Panel!");
+    chrome.devtools.network.onRequestFinished.addListener(function (request) {
+      // request.getContent(function (content, encoding) {
+      request.getContent(function (content) {
+        if (isJsonString(content)) {
+          var myobj = JSON.parse(content);
+          console.log(myobj.question);
+          if (myobj.question) {
+            let i = findCorrectAns(myobj.question.answers);
+            console.log("Correct Answer is: " + i);
+            chrome.tabs.query(
+              { active: true, currentWindow: true },
+              function (tabs) {
+                var activeTab = tabs[0];
+                //Sending message to the active tab
+                chrome.tabs.sendMessage(activeTab.id, {
+                  msg: "Sending Data",
+                  keys: i,
+                });
+              }
+            );
+          }
         }
+      });
+    });
+  }
+});
+
+function isJsonString(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+function findCorrectAns(answers) {
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i].isCorrectAnswer) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
